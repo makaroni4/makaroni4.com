3f61b5b35a2a78e3ae1dcdc8d3b302b38fb8d0d4
Anatoli Makarevich <makaroni4@gmail.com>
2019-01-07 23:17:03 +0100

    Concat JS files to one, drop avatar easter egg

---
+++ js/app.js
@@ -0,0 +1,10 @@
+---
+---
+
+{% include_relative jquery.js %}
+{% include_relative helpers.js %}
+{% include_relative link_decoration.js %}
+{% include_relative tweet.js %}
+{% include_relative image_popup.js %}
+{% include_relative projects_page.js %}
+{% include_relative newsletter_form.js %}
