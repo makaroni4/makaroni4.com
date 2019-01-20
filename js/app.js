bb34ba224687fbd0dacdacf1dc47a510b08af4de
Anatoli Makarevich <makaroni4@gmail.com>
2019-01-20 16:03:02 +0100

    Drop unused JS, async for app.js

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@
 
 {% include_relative jquery.js %}
 {% include_relative helpers.js %}
-{% include_relative link_decoration.js %}
 {% include_relative tweet.js %}
 {% include_relative image_popup.js %}
 {% include_relative projects_page.js %}
