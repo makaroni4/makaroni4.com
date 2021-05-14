7f5f3fcc7110071881343a8e9e7e4a15c154bdee
Anatoli Makarevich <makaroni4@gmail.com>
2021-05-14 20:29:30 +0200

    Stylin stylin stylin

--- src/main.js
+++ src/main.js
@@ -4,9 +4,15 @@ import App from "./App.vue";
 import "./modules/tweet";
 
 import "./assets/css/design-system.scss";
+
 import "./assets/css/jekyll-components/avatar-hero.scss";
 import "./assets/css/jekyll-components/header.scss";
 import "./assets/css/jekyll-components/footer.scss";
+import "./assets/css/jekyll-components/post-list.scss";
+import "./assets/css/jekyll-components/project-list.scss";
+import "./assets/css/jekyll-components/project.scss";
+import "./assets/css/jekyll-components/projects-page.scss";
+
 import "./assets/css/vendor/twemoji.scss";
 
 createApp(App).mount("#blog-vue-app");
