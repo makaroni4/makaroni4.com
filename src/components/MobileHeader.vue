9a43d2e359133d2d1d2c272b551fc1f1b14d2a63
Anatoli Makarevich <makaroni4@gmail.com>
2021-05-20 22:08:23 +0200

    Added mobile dropdown menu

--- src/components/MobileHeader.vue
+++ src/components/MobileHeader.vue
@@ -1,12 +1,42 @@
 <template>
   <header class="mobile-header visible-xs">
     <div class="mobile-header__container">
-      <div class="mobile-header__menu">
+      <div class="mobile-header__menu-bar">
         <div class="mobile-header__logo font-small sans-serif">
           makaroni4.com
         </div>
 
-        <BarIcon class="mobile-header__bar-icon" />
+        <CloseIcon
+          v-if="menuShown"
+          @click.prevent="menuShown = !menuShown"
+          class="mobile-header__toggle-icon" />
+
+        <BarIcon
+          v-else
+          @click.prevent="menuShown = !menuShown"
+          class="mobile-header__toggle-icon" />
+      </div>
+
+      <div
+        v-if="menuShown"
+        class="mobile-header__dropdown">
+        <a
+          class="mobile-header__dropdown-link font-large sans-serif no-underline"
+          href="/articles">
+          ARTICLES
+        </a>
+
+        <a
+          class="mobile-header__dropdown-link font-large sans-serif no-underline"
+          href="/articles">
+          PROJECTS
+        </a>
+
+        <a
+          class="mobile-header__dropdown-link font-large sans-serif no-underline"
+          href="/articles">
+          NOW
+        </a>
       </div>
     </div>
   </header>
@@ -14,11 +44,18 @@
 
 <script>
 import BarIcon from '@/assets/images/bars-solid.svg';
+import CloseIcon from '@/assets/images/times-solid.svg';
 
 export default {
   name: 'MobileHeader',
   components: {
     BarIcon,
+    CloseIcon,
+  },
+  data() {
+    return {
+      menuShown: false,
+    };
   },
 };
 </script>
@@ -26,10 +63,14 @@ export default {
 <style lang="scss">
 .mobile-header {
   padding: $px24;
+  position: relative;
 
   background-color: $purple-5;
 
-  &__menu {
+  &__container {
+  }
+
+  &__menu-bar {
     display: flex;
     align-items: center;
     justify-content: space-between;
@@ -39,10 +80,32 @@ export default {
     color: $white;
   }
 
-  &__bar-icon {
-    width: $px24;
+  &__toggle-icon {
+    height: $px24;
 
     color: $white;
+  }
+
+  &__dropdown {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    padding: $px32 $px24;
+    z-index: 100;
+    transform: translateY(100%);
+
+    background-color: $purple-5;
+  }
+
+  &__dropdown-link {
+    color: $white;
+
+    &:not(:last-child) {
+      margin-bottom: $px24;
+    }
   }
 }
 </style>
