00b556089c77a8f7f1aba66944b7ac72f0bbb081
Anatoli Makarevich <makaroni4@gmail.com>
2025-03-03 22:01:55 +0100

    Added Vite, latest Vue

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import { createApp } from 'vue';
+import App from './App.vue';
 import twemoji from 'twemoji';
 
 import './controllers/mobile-header';
@@ -7,6 +9,13 @@ import './modules/disqus-comments';
 
 import './assets/css/app.scss';
 
+// Create the Vue application
+const app = createApp(App);
+
+// Mount the app
+app.mount('#app');
+
+// Apply twemoji after app mounting
 twemoji.parse(document.body, {
   folder: 'svg',
   ext: '.svg',
