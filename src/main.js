635c1b538b7f02b04bc9208eaa9c4cbd619b62d8
Anatoli Makarevich <makaroni4@gmail.com>
2025-04-07 09:13:26 +0200

    Added Redirect to FY LP for Chrome Store visits

--- src/main.js
+++ src/main.js
@@ -7,6 +7,11 @@ import './modules/disqus-comments';
 
 import './assets/css/app.scss';
 
+// Since we can't set makaroni4.com/focused-youtube as  an extension URL in Chrome Store, let's check if a visitor is coming from Chrome Web Store and redirect to Focused YouTube page:
+if (document.referrer.includes('chromewebstore.google.com')) {
+  window.location.href = '/focused-youtube';
+}
+
 twemoji.parse(document.body, {
   folder: 'svg',
   ext: '.svg',
